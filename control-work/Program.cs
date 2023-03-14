586926b1daa4b0a8e9d0b4b1568f33fc53a8cba2
Stanislav Shishkin <shishkinstas@mail.ru>
2023-03-14 12:25:30 +0700

    Задали первоначальный массив

--- control-work/Program.cs
+++ control-work/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string array = ["итоговая", "проверочная", "работа", "1", "три"];
