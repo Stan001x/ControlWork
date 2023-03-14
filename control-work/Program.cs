8277d50d31a74e710de6efc24cfd930460583734
Stanislav Shishkin <shishkinstas@mail.ru>
2023-03-14 12:50:32 +0700

    исправили ошибку, вывели исходный массив на экран

--- control-work/Program.cs
+++ control-work/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.Clear();
 
-string array = ["итоговая", "проверочная", "работа", "1", "три"];
+string[] array = {"итоговая", "проверочная", "работа", "1", "три"};
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
